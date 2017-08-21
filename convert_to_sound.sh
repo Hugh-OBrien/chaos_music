6bf4a6c669e8ab848f072db9a194db0583245c49
hugh-obrien <hugh.obrien@overleaf.com>
2017-08-21 17:25:04 +0100

    Basic convert from image to sound

--- convert_to_sound.sh
+++ convert_to_sound.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-ffmpeg -y -i \"$1\" -pix_fmt $YUV_FMT $FFMPEG_IN_OPTS  $TMP_DIR/tmp.yuv
+echo "Extract raw image data..."
+echo $1
+# convert to yuv colour space
+ffmpeg -y -i $1 -pix_fmt rgb24 ./tmp.yuv
 
-[[ $AUDIO = *[!\ ]* ]] && echo "Extracting audio track.."
-[[ $AUDIO = *[!\ ]* ]] && cmdSilent "ffmpeg -y -i \"$1\" -q:a 0 -map a $TMP_DIR/audio_in.${AUDIO_TYPE}"
+mv tmp.yuv tmp.u8
+# convert to sound with sox
+sox tmp.u8 out.wav
 
-echo "Processing as sound.."
-mv "$TMP_DIR"/tmp.yuv "$TMP_DIR"/tmp_audio_in."$S_TYPE"
-cmdSilent sox --bits "$BITS" -c1 -r44100 --encoding unsigned-integer -t "$S_TYPE" "$TMP_DIR"/tmp_audio_in."$S_TYPE"  \
-              --bits "$BITS" -c1 -r44100 --encoding unsigned-integer -t "$S_TYPE" "$TMP_DIR"/tmp_audio_out."$S_TYPE" \
-              "$SOX_OPTS"
+
+echo "Recreate image..."
+# ffmpeg -y -i ./tmp.yuv \"$2\"
